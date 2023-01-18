91dcfd9f37e843481e748e467c1c4d5956400105
Léo Guillaume <leo.guillaume@gmail.com>
2023-01-18 13:59:26 +0100

    up

--- izigit.sh
+++ izigit.sh
@@ -57,19 +57,19 @@ PrIssueToPreprod() {
 
     git status
 
-    # Check if there are any conflicts
-    #disable lf and crlf warnings
-    git config core.autocrlf false
-    if git diff --name-only --diff-filter=U | grep -q "^"; then
-      echo "There are merge conflicts to resolve :"
-      git diff --name-only --diff-filter=U
-      exit 0
-    fi
-
-    # Get issue title
-    pr_title=$(gh issue view $2 --json title --jq .title)
-    # Create a pull request from the issue branch to the target branch (preprod)
-    gh pr create --base preprod --head $issue_branch_name -t "$pr_title" -b ""
+#    # Check if there are any conflicts
+#    #disable lf and crlf warnings
+#    git config core.autocrlf false
+#    if git diff --name-only --diff-filter=U | grep -q "^"; then
+#      echo "There are merge conflicts to resolve :"
+#      git diff --name-only --diff-filter=U
+#      exit 0
+#    fi
+#
+#    # Get issue title
+#    pr_title=$(gh issue view $2 --json title --jq .title)
+#    # Create a pull request from the issue branch to the target branch (preprod)
+#    gh pr create --base preprod --head $issue_branch_name -t "$pr_title" -b ""
 
     # Add comment to issue
     comment="$date - create pull request branch $issue_branch_name to $preprod_branch - $github_name"
