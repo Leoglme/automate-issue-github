2605676cf9a6cbe8ef4d5f172537abd61c47cc77
Léo Guillaume <leo.guillaume@gmail.com>
2023-01-18 10:51:03 +0100

    fix test script

--- izigit.sh
+++ izigit.sh
@@ -31,7 +31,7 @@ Help() {
   echo ""
   echo "create [ticket_number] Create, fetch and checkout branch for ticket ( example: izigit create 654 )"
   echo "reset Reset test branch, so that it is strictly identical to the preprod branch ( example: izigit reset )"
-  echo "test Merge issue branch into test branch ( example: izigit test )"
+  echo "test [ticket_number] Merge issue branch into test branch ( example: izigit test 654 )"
   echo "pr Creating a pull request from the github issue branch to the preprod branch ( example: izigit pr )"
   echo "h     Print this Help."
 }
@@ -44,76 +44,46 @@ PrIssueToPreprod() {
   gh pr create --base preprod --head $current_branch
 }
 
-# Fonction for merge issue branch into test branch //ticket number
+# Fonction for merge issue branch into test branch
+# Arguments: $2 = ticket number
 MergeIssueInTest() {
-  #  # Get branch name with ticket number
-  #  branch_name=$(git branch -r | grep $2 | sed 's/origin\///')
-  #
-  #  if [ $branch_name ]
-  #  then
-  #    echo "No git branch found for ticket 703"
-  #    exit 0
-  #  fi
+  if [ $2 ]; then
+    # Get branch name with ticket number
+    branch_name=$(git branch -r | grep $2 | sed 's/origin\///')
 
-  #  git checkout test
-  #  git pull origin test
-  #  git merge $current_branch
+    if [ $branch_name ]; then
+      echo "No git branch found for ticket $2"
+      exit 0
+    fi
 
-  #  # Configurer les variables d'environnement
-  #  REPO=sportoutdoor
-  #  COLUMN_ID_FROM=COLUMN_ID_FROM
-  #  COLUMN_ID_TO=COLUMN_ID_TO
-  #  CARD_ID=738
-  #  GITHUB_TOKEN=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
-  #
-  #  # Déplacer la carte vers une autre colonne
-  #  curl -X POST \
-  #    -H "Accept: application/vnd.github+json" \
-  #    -H "Authorization: Bearer $GITHUB_TOKEN" \
-  #    -H "Content-Type: application/json" \
-  #    -d "{\"column_id\": $COLUMN_ID_TO}" \
-  #    https://api.github.com/projects/columns/$COLUMN_ID_FROM/cards/$CARD_ID/moves
-
-  gh api graphql -f query='
-    query{
-      organization(login: "sportoutdoor"){
-        projectV2(number: 1) {
-          id
-          title
-        }
-      }
-    }'
-  gh api graphql -f query='
-    query{
-        node(id: "PVT_kwDOBrk6n84AFqb8") {
-             id,
-             columns
-             }
-      }'
-
+    git checkout test
+    git pull origin test
+    git merge $current_branch
+  fi
+  echo "Please specify allows ticket number, izigit test [ticket_number] ( example: izigit test 654 )"
 }
 
 # Fonction for reset test branch, so that it is strictly identical to the preprod branch
 ResetTestToPreprod() {
-    test_branch="test"
-    reference_branch="preprod"
+  test_branch="test"
+  reference_branch="preprod"
 
-    # Fetch and checkout the reference branch (preprod)
-    git fetch
-    git checkout $reference_branch
-    git pull $reference_branch
+  # Fetch and checkout the reference branch (preprod)
+  git fetch
+  git checkout $reference_branch
+  git pull $reference_branch
 
-    # Delete the local working branch
-    git branch -D $test_branch
+  # Delete the local working branch
+  git branch -D $test_branch
 
-    # Create a new branch from the reference branch (preprod)
-    git checkout -b $test_branch
+  # Create a new branch from the reference branch (preprod)
+  git checkout -b $test_branch
 
-    # Push the new branch to the remote repository
-    git push -u origin $test_branch --force
+  # Push the new branch to the remote repository
+  git push -u origin $test_branch --force
 
-    comment="$date - $test_branch reset to $reference_branch - $github_name"
-    echo $comment
+  comment="$date - $test_branch reset to $reference_branch - $github_name"
+  echo $comment
 }
 
 # Fonction for create branch for ticket and fetch, checkout this branch
